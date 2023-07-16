78e9310a40daa3bfa8dc3848252adc83b576a6a6
neppord <neppord@gmail.com>
2023-07-16 12:56:16 +0200

    Fix tests

--- foreign/test_data_foldable.py
+++ foreign/test_data_foldable.py
@@ -1,24 +1,28 @@
-from corefn.abs import Foreign
+from corefn.abs import Foreign, Native3, Native2
 from corefn.literals import Array, Int
 from foreign import foldr_array, int_add, to_foreign
 from foreign.data_foldable import foldr_array_
 
 
 def test_foldr_array():
-    assert foldr_array_(None, to_foreign(int_add), Int(0), Array([
+    assert foldr_array_(None, Native2(int_add), Int(0), Array([
         Int(1),
         Int(2),
         Int(3),
     ])).value == 6
 
-    assert foldr_array_(None, to_foreign(lambda a, b: b), Int(0), Array([
+    assert foldr_array_(None, to_foreign(lambda i, a, b: b), Int(0), Array([
         Int(1),
         Int(2),
         Int(3),
     ])).value == 0
 
-    assert foldr_array_(None, Foreign("", lambda a: Foreign("", lambda b: a)), Int(0), Array([
+def test_foldr_array_first():
+    assert foldr_array_(None, Native2(first), Int(0), Array([
         Int(1),
         Int(2),
         Int(3),
     ])).value == 1
+
+def first(i, a, b):
+    return a
