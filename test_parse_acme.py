8d298fcab2d3eb88c411cb50bb8237a0be581e2e
neppord <neppord@gmail.com>
2023-07-18 21:52:59 +0200

    Skipp long running tests

--- test_parse_acme.py
+++ test_parse_acme.py
@@ -19,11 +19,15 @@ files = {os.path.relpath(f) for glob_expression in [
     os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "test", "*", "*", "*", "*", "*.purs"),
 ] for f in glob.glob(glob_expression)}
 skip = {
-    os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "FontAwesome.purs"),
+    os.path.join("e2e", "acme", ".spago", "unicode", "v6.0.0", "src", "Data", "CodePoint", "Unicode", "Internal.purs"),
     os.path.join("e2e", "acme", ".spago", "unicode", "v6.0.0", "src", "Data", "CodePoint", "Unicode", "Internal", "Casing.purs"),
-    os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "TablerIcons.purs"),
     os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "FontAwesome.purs"),
+    os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "TablerIcons.purs"),
     os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "Primer.purs"),
+    os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "RemixIcon.purs"),
+    os.path.join("e2e", "acme", ".spago", "react-basic-dom", "v6.1.0", "src", "React", "Basic", "DOM", "Simplified", "Generated.purs"),
+    os.path.join("e2e", "acme", ".spago", "react-basic-dom", "v6.1.0", "src", "React", "Basic", "DOM", "Generated.purs"),
+    os.path.join("e2e", "acme", ".spago", "elmish-html", "v0.8.1", "src", "Elmish", "HTML", "Generated.purs"),
 }
 
 assert files
