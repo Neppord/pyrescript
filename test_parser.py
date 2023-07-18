addcefd554a6326948d4e77b9af7f796dc88b317
neppord <neppord@gmail.com>
2023-07-18 08:51:18 +0200

    Move acme test into its own test module

--- test_parser.py
+++ test_parser.py
@@ -109,30 +109,3 @@ def test_parse_module_with_do():
     property_name, = module_name.children
     assert property_name.token.source == "Main"
 
-
-files = [os.path.relpath(f) for glob_expression in [
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "src", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "src", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "src", "*", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "src", "*", "*", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "src", "*", "*", "*", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "test", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "test", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "test", "*", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "test", "*", "*", "*", "*.purs"),
-    os.path.join(os.path.dirname(__file__), "e2e", "acme", ".spago", "*", "*", "test", "*", "*", "*", "*", "*.purs"),
-] for f in glob.glob(glob_expression)]
-
-assert files
-
-
-@pytest.mark.parametrize("file_path", files)
-def test_e2e(file_path):
-    with open(file_path) as source_file:
-        source = source_file.read()
-    tokens = lexer.tokenize_with_name(file_path, source)
-    try:
-        module_parser.parse(tokens)
-    except ParseError as e:
-        message = e.nice_error_message(file_path, source, tokens)
-        raise ValueError(message)
