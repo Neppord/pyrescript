cc9530527f72eae232b970436123b8585ba0e9a7
neppord <neppord@gmail.com>
2023-07-18 22:01:56 +0200

    Skipp long running tests

--- test_parse_acme.py
+++ test_parse_acme.py
@@ -25,6 +25,8 @@ skip = {
     os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "TablerIcons.purs"),
     os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "Primer.purs"),
     os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "RemixIcon.purs"),
+    os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "BootstrapIcons.purs"),
+    os.path.join("e2e", "acme", ".spago", "css-frameworks", "v1.0.1", "src", "CSSFrameworks", "BoxIcons.purs"),
     os.path.join("e2e", "acme", ".spago", "react-basic-dom", "v6.1.0", "src", "React", "Basic", "DOM", "Simplified", "Generated.purs"),
     os.path.join("e2e", "acme", ".spago", "react-basic-dom", "v6.1.0", "src", "React", "Basic", "DOM", "Generated.purs"),
     os.path.join("e2e", "acme", ".spago", "elmish-html", "v0.8.1", "src", "Elmish", "HTML", "Generated.purs"),
